be12abdb749a33d8cdac0e8677a846507828af5e
Matteo Antoci <matteo.antoci@lastminute.com>
2021-04-28 21:55:32 +0200

    Improved graceful exit

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,18 @@ import { schedule } from 'node-cron'
 import { SETTINGS } from './settings'
 import { CapRoverClient, createCapRoverClient } from './capRoverClient'
 import { backup, initializeRepository } from './tasks'
+import { shellExec, shellLog } from './shell'
 
-const exit = (err: Error) => {
-  console.error('>>> An unexpected error occurred!', err.stack)
+const exit = async (err: Error) => {
+  console.error('❌ An unexpected error occurred!', err.stack)
+
+  console.info('ℹ️  Exiting...')
+  await shellExec('restic unlock')
+    .then(shellLog)
+    .catch((err) => {
+      console.error('❌ Cannot remove repository locks', err.stack)
+    })
+
   process.exit(1)
 }
 
@@ -13,17 +22,17 @@ const startCronJob = (client: CapRoverClient) => {
   schedule(cronExpression, () => {
     backup(client).catch(exit)
   })
-  console.info(`>>> "${cronExpression}" cron started`)
+  console.info(`✅ "${cronExpression}" cron started`)
 }
 
 const start = async () => {
-  console.info(`>>> Starting Caprover backup server...`)
+  console.info(`ℹ️ Starting Caprover backup server...`)
 
   await initializeRepository()
 
   startCronJob(createCapRoverClient(SETTINGS.capRover.publicUrl))
 
-  console.info('>>> Caprover backup server started!')
+  console.info('✅ Caprover backup server started!')
 }
 
 start().catch(exit)
